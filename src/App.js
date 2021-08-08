cc207c6bf5fd6a953414f471f24b66732e562424
Henil Shah <henil.601@gmail.com>
2021-08-07 18:12:55 -0700

    Completed Navbar and added more components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
-import About from "./components/About";
+import Education from "./components/Education";
+import Work from "./components/Work";
 import SingleProject from "./components/SingleProject";
 import Project from "./components/Project";
+import Achievements from "./components/Achievements";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<Router>
+			<Navbar />
 			<Switch>
 				<Route component={Home} exact path="/" />
-				<Route component={About} path="/about" />
+				<Route component={Education} path="/education" />
+				<Route component={Work} path="/work" />
 				<Route component={SingleProject} path="/project/:slug" />
 				<Route component={Project} path="/project" />
+				<Route component={Achievements} path="/achievements" />
 			</Switch>
 		</Router>
 	);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,61 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+import { faAward } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
 	return (
-		<div>
-			<h1>Navbar</h1>
-		</div>
+		<header className="bg-black">
+			<div className="container mx-auto flex justify-around">
+				<nav style={{ fontSize: "18px" }} className="flex myFont">
+					<NavLink
+						to="/"
+						exact
+						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						activeClassName="text-white hover:text-gray-900"
+					>
+						<FontAwesomeIcon icon={faUserCircle} />
+						<p className="text-sm">About Me</p>
+					</NavLink>
+					<NavLink
+						to="/education"
+						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						activeClassName="text-white hover:text-gray-900"
+					>
+						<FontAwesomeIcon icon={faGraduationCap} />
+						<p className="text-sm">Education</p>
+					</NavLink>
+					<NavLink
+						to="/work"
+						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						activeClassName="text-white hover:text-gray-900"
+					>
+						<FontAwesomeIcon icon={faBuilding} />
+						<p className="text-sm">Work</p>
+					</NavLink>
+					<NavLink
+						to="/project"
+						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						activeClassName="text-white hover:text-gray-900"
+					>
+						<FontAwesomeIcon icon={faLaptopCode} />
+						<p className="text-sm">Projects</p>
+					</NavLink>
+					<NavLink
+						to="/achievements"
+						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						activeClassName="text-white hover:text-gray-900"
+					>
+						<FontAwesomeIcon icon={faAward} />
+						<p className="text-sm">Achievements</p>
+					</NavLink>
+				</nav>
+			</div>
+		</header>
 	);
 };
 
