a36f6287789453fc59189206a59da21e51d428c9
Henil Shah <henil.601@gmail.com>
2021-08-11 19:45:15 -0700

    Fixed Navbar with responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,47 +11,69 @@ const Navbar = () => {
 	return (
 		<header className="bg-black">
 			<div className="container mx-auto flex justify-around">
-				<nav style={{ fontSize: "18px" }} className="flex myFont">
+				<nav className="flex myFont text-gray-500 hover:text-white hover:no-underline">
 					<NavLink
 						to="/"
 						exact
-						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						className="text-center py-3 px-3 mr-2 my-4 sm:my-6 sm:text-3xl md:my-6 md:text-xl lg:my-6 lg:text-xl"
 						activeClassName="text-white hover:text-gray-900"
 					>
-						<FontAwesomeIcon icon={faUserCircle} />
-						<p className="text-sm">About Me</p>
+						<FontAwesomeIcon
+							className="text-lg"
+							icon={faUserCircle}
+						/>
+						<p className="sm:text-xl md:block lg:block hidden">
+							About Me
+						</p>
 					</NavLink>
 					<NavLink
 						to="/education"
-						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						className="text-center py-3 px-3 mr-2 my-4 sm:my-6 sm:text-3xl md:my-6 md:text-xl lg:my-6 lg:text-xl"
 						activeClassName="text-white hover:text-gray-900"
 					>
-						<FontAwesomeIcon icon={faGraduationCap} />
-						<p className="text-sm">Education</p>
+						<FontAwesomeIcon
+							className="text-lg"
+							icon={faGraduationCap}
+						/>
+						<p className="sm:block md:block lg:block hidden">
+							Education
+						</p>
 					</NavLink>
 					<NavLink
 						to="/work"
-						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						className="text-center py-3 px-3 mr-2 my-4 sm:my-6 sm:text-3xl md:my-6 md:text-xl lg:my-6 lg:text-xl"
 						activeClassName="text-white hover:text-gray-900"
 					>
-						<FontAwesomeIcon icon={faBuilding} />
-						<p className="text-sm">Work</p>
+						<FontAwesomeIcon
+							className="text-lg"
+							icon={faBuilding}
+						/>
+						<p className="sm:block md:block lg:block hidden">
+							Work
+						</p>
 					</NavLink>
 					<NavLink
 						to="/project"
-						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						className="text-center py-3 px-3 mr-2 my-4 sm:my-6 sm:text-3xl md:my-6 md:text-xl lg:my-6 lg:text-xl"
 						activeClassName="text-white hover:text-gray-900"
 					>
-						<FontAwesomeIcon icon={faLaptopCode} />
-						<p className="text-sm">Projects</p>
+						<FontAwesomeIcon
+							className="text-lg"
+							icon={faLaptopCode}
+						/>
+						<p className="sm:block md:block lg:block hidden">
+							Projects
+						</p>
 					</NavLink>
 					<NavLink
 						to="/achievements"
-						className="text-center inflex-flex items-center py-3 px-3 my-6 mr-2 text-3xl text-gray-500 hover:text-white hover:no-underline"
+						className="text-center py-3 px-3 mr-2 my-4 sm:my-6 sm:text-3xl md:my-6 md:text-xl lg:my-6 lg:text-xl"
 						activeClassName="text-white hover:text-gray-900"
 					>
-						<FontAwesomeIcon icon={faAward} />
-						<p className="text-sm">Achievements</p>
+						<FontAwesomeIcon className="text-lg" icon={faAward} />
+						<p className="sm:block md:block lg:block hidden">
+							Achievements
+						</p>
 					</NavLink>
 				</nav>
 			</div>
